b96dfee7fc0066ffea30942f5d313ad957e9d884
Jason Burke <jpburke77@gmail.com>
2023-03-07 02:24:07 -0600

    win tie alerts completed player[ ] reset completed

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ const playerFactory = (playerNumber, playerIcon = null, lastMove = false) => {
   const number = playerNumber;
   const icon = playerIcon;
   let madeLastMove = lastMove;
+  console.log({ number, icon, madeLastMove });
   return { number, icon, madeLastMove };
 };
 
 const gameBoard = (() => {
   const gameBoardSquares = document.querySelectorAll('.piece');
   let board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  let players = [];
 
-  characterArray = [
+  const characterArray = [
     'Bender',
     'Fry',
     'Leela',
@@ -21,10 +23,6 @@ const gameBoard = (() => {
     'Nibbler',
   ];
 
-  // const player1 = playerFactory(1);
-  // const player2 = playerFactory(2, './images/Zoidberg.ico', true);
-  let players = [];
-
   chooseCharacterPara = document.querySelector('.choose-characters');
   newGameBtn = document.querySelector('.new-game');
   newGameBtn.addEventListener('click', () => {
@@ -33,12 +31,18 @@ const gameBoard = (() => {
     pickCharacter();
   });
 
+  playAgainBtn = document.querySelector('.play-again');
+  playAgainBtn.addEventListener('click', () => {
+    playAgainBtn.classList.add('hidden');
+    document.querySelector('.winner').classList.add('hidden');
+    document.querySelector('.tie').classList.add('hidden');
+    document.querySelector('.choose-characters').classList.remove('hidden');
+    resetBoard();
+  });
+
   function pickCharacter() {
     gameBoardSquares.forEach((square) => {
-      square.addEventListener('click', (event) => {
-        // event.currentTarget.style.pointerEvents = 'none';
-        setPlayer(event);
-      });
+      square.addEventListener('click', setPlayer);
     });
   }
 
@@ -56,27 +60,34 @@ const gameBoard = (() => {
       gameBoardSquares.forEach((square) => {
         square.style.pointerEvents = 'auto';
       });
-      clearBoard();
+      document.querySelector('.choose-characters').classList.add('hidden');
+      setTimeout(clearBoard, 250);
     }
   }
 
   function clearBoard() {
     gameBoardSquares.forEach((square, i) => {
+      square.childNodes[1].style.opacity = '1';
+
       square.childNodes[1].setAttribute('src', '');
     });
-    newGame = true;
   }
 
   function resetBoard() {
     board.fill(0);
+    players = [];
     gameBoardSquares.forEach((square, i) => {
+      square.removeEventListener('click', setPlayer);
+      square.style.scale = 1;
+      square.style.backgroundColor = 'rgba(75, 41, 123, 0.575)';
       square.style.pointerEvents = 'auto';
+      square.style.opacity = '1';
       square.childNodes[1].setAttribute(
         'src',
         `./images/${characterArray[i]}.ico`
       );
     });
-    newGame = true;
+    pickCharacter();
   }
 
   const winArray = [
@@ -93,23 +104,39 @@ const gameBoard = (() => {
   function checkForWinner() {
     winArray.forEach((winCheck) => {
       let bSquares = board.filter((el, i) => winCheck.some((j) => i === j));
-      console.log('winCheck: ', winCheck);
-      console.log('bSquares', bSquares);
       if (bSquares.every((val, i, arr) => val !== 0 && val === arr[0])) {
+        console.log('here');
         alertWinner(winCheck);
       }
+    });
+    if (!board.includes(0)) {
+      console.log('also here');
+      alertTie();
+    }
+  }
+
+  function alertTie() {
+    gameBoardSquares.forEach((square) => {
+      square.style.pointerEvents = 'none';
+      setTimeout(() => {
+        document.querySelector('.tie').classList.remove('hidden');
+        document.querySelector('.play-again').classList.remove('hidden');
+      }, 100);
     });
   }
 
   function alertWinner(winArray) {
+    board[1] = 0;
     gameBoardSquares.forEach((square) => {
       square.style.pointerEvents = 'none';
       squareIndex = parseInt(square.getAttribute('index'));
-      console.log(typeof squareIndex);
       if (winArray.includes(squareIndex)) {
-        console.log(square.getAttribute('index'));
         square.style.scale = 1.1;
         square.style.backgroundColor = 'rgba(248,248,255, .5)';
+        setTimeout(() => {
+          document.querySelector('.winner').classList.remove('hidden');
+          document.querySelector('.play-again').classList.remove('hidden');
+        }, 100);
       }
     });
   }
@@ -142,72 +169,8 @@ const gameBoard = (() => {
       });
     }
   }
-
-  return { resetBoard, gameBoardSquares, clearBoard, board };
+  return { gameBoardSquares, board };
 })();
-
-// gameBoard.pickCharacter();
-
-// const gameController = ((gameBoardSquares) => {
-//   console.log(gameBoardSquares);
-//   function checkForWinner() {
-//     // board.slice(0, 3).every((val, i, arr) => val === arr[0]) // {0,1,2}
-//     //   ? alert('winner')
-//     //   : board.slice(3, 6).every((val, i, arr) => val === arr[0]) // {3,4,5}
-//     //   ? alert('winner')
-//     //   : board.slice(6, 9).every((val, i, arr) => val === arr[0]) // {6,7,8}
-//     //   ? alert('winner')
-//     //   : console.log('no winner');
-//     // board.slice(0, 3).every((val, arr) => val === arr[0]) ? alert('winner') : {0,3,6}
-//     // board.slice(0, 3).every((val, arr) => val === arr[0]) ? alert('winner') : {1,4,7}
-//     // board.slice(0, 3).every((val, arr) => val === arr[0]) ? alert('winner') : {2,5,8}
-//     // board.slice(0, 3).every((val, arr) => val === arr[0]) ? alert('winner') : {2,4,6}
-//     // board.slice(0, 3).every((val, arr) => val === arr[0]) ? alert('winner') : {0,4,8}
-//   }
-
-//   gameBoardSquares.forEach((square) => {
-//     square.addEventListener('click', (event) => {
-//       playerMove(event, players);
-//     });
-//   });
-
-//   function playerMove(event, players) {
-//     square = event.currentTarget;
-//     console.log(players);
-//     if (board[square.getAttribute('index')] === 0) {
-//       players.forEach((player) => {
-//         if (player.madeLastMove) {
-//           player.madeLastMove = false;
-//         } else {
-//           board[square.getAttribute('index')] = player.number;
-//           square.childNodes[1].setAttribute('src', player.icon);
-//           player.madeLastMove = true;
-//         }
-//       });
-//     }
-//   }
-// })();
-
-// const playGame = ((player1, player2) => {
-//   let winner = false;
-//   // while (!winner) {
-//   function play(player1, player2) {
-//     gameBoard.addMove(player1);
-//     console.log('here');
-//     gameBoard.addMove(player2);
-//   }
-//   // }
-//   return { player1, player2, play };
-// })();
-
-// const player1 = playerFactory(1, './images/Nibbler.ico');
-// // gameBoard.addMove(player1);
-// const player2 = playerFactory(2, './images/Zoidberg.ico');
-// gameBoard.addMove(player1);
-// gameBoard.addMove(player2);
-// playGame.play(player1, player2);
-// gameBoard.playerMove(player1);
-// gameBoard.playerMove(player2);
 
 // mouse tracking code
 gameBoard.gameBoardSquares.forEach((square) => {
